56046b11ceec38b349e7a8018377602d875d843c
ZED-Magdy <zedmagdy@gmail.com>
2021-08-04 09:41:14 +0200

    Add tests for Order/Offer

---
+++ tests/Domain/Model/OfferTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests\Domain\Model;
+
+use App\Domain\Model\Client;
+use App\Domain\Model\Driver;
+use App\Domain\Model\Offer;
+use App\Domain\Model\Place;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+class OfferTest extends TestCase
+{
+
+    public function testCreate()
+    {
+        $driver = Driver::create("driver@example.com", "ey$.2312313");
+        $client = Client::create("client@example.com", "ey$.aasdadad1223");
+        $order = $client->makeOrder(Uuid::uuid4()->toString(), new Place("McDonald", "1231", "1231", "street"),
+            new Place("Home", "123131", "12313212", "home"), null);
+        $client->addProductToOrder($order, "Big Mac", "2");
+        $client->publishOrder($order);
+        $offer = new Offer(Uuid::uuid4()->toString(), $driver, $order, "50");
+        $this->assertInstanceOf(Offer::class, $offer);
+    }
+
+    public function testMarkAsAccepted()
+    {
+        $driver = Driver::create("driver@example.com", "ey$.2312313");
+        $client = Client::create("client@example.com", "ey$.aasdadad1223");
+        $order = $client->makeOrder(Uuid::uuid4()->toString(), new Place("McDonald", "1231", "1231", "street"),
+            new Place("Home", "123131", "12313212", "home"), null);
+        $client->addProductToOrder($order, "Big Mac", "2");
+        $client->publishOrder($order);
+        $offer = new Offer(Uuid::uuid4()->toString(), $driver, $order, "50");
+        $offer->markAsAccepted($client);
+        $this->assertNotNull($offer->getAcceptedAt());
+    }
+}
--- tests/Domain/Model/OrderTest.php
+++ tests/Domain/Model/OrderTest.php
@@ -2,39 +2,118 @@
 
 namespace App\Tests\Domain\Model;
 
+use App\Domain\Exception\InvalidActionForCurrentOrderState;
+use App\Domain\Model\Client;
+use App\Domain\Model\Driver;
 use App\Domain\Model\Order;
+use App\Domain\Model\Place;
+use App\Domain\Model\Product;
 use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
 
 class OrderTest extends TestCase
 {
+    public function testCreate()
+    {
+        $client = Client::create("client@example.com", "ey$.sadasd123asd123");
+        $order = new Order(Uuid::uuid4()->toString(),
+            new Place("McDonald","123.123","321.321","McDonald st"),
+            new Place("Home","321.321","123.123","Home st"),
+            $client,"");
+        $this->assertInstanceOf(Order::class, $order);
+    }
+    public function testAddProduct()
+    {
+        $client = Client::create("client@example.com", "ey$.sadasd123asd123");
+        $order = new Order(Uuid::uuid4()->toString(),
+            new Place("McDonald","123.123","321.321","McDonald st"),
+            new Place("Home","321.321","123.123","Home st"),
+            $client,"");
+        $product = $order->addProduct($client, "new product", "5");
+        $this->assertInstanceOf(Product::class, $product);
+        $this->assertCount(1, $order->getProducts());
+    }
 
     public function testMarkAsPublishedByClient()
     {
+        $client = Client::create("client@example.com", "ey$.sadasd123asd123");
+        $order = new Order(Uuid::uuid4()->toString(),
+            new Place("McDonald","123.123","321.321","McDonald st"),
+            new Place("Home","321.321","123.123","Home st"),
+            $client,"");
+        $product = $order->addProduct($client, "new product", "5");
+        $order->markAsPublishedByClient($client);
 
+        $this->assertEquals(Order::STATUS_PENDING, $order->getStatus());
+        $this->assertNotNull($order->getPublishedAt());
     }
-
-    public function testAddProduct()
-    {
-
-    }
-
-    public function testMarkAsDelivered()
-    {
-
-    }
-
     public function testMarkAsProcessing()
     {
+        $client = Client::create("client@example.com", "ey$.sadasd123asd123");
+        $order = new Order(Uuid::uuid4()->toString(),
+            new Place("McDonald","123.123","321.321","McDonald st"),
+            new Place("Home","321.321","123.123","Home st"),
+            $client,"");
+        $product = $order->addProduct($client, "new product", "5");
+        $order->markAsPublishedByClient($client);
+        $driver = Driver::create("driver@example.com", "ey$.2312313");
+        $offer = $driver->makeOffer($order, "55");
+        $order->markAsProcessing($client, $offer);
+        $this->assertEquals(Order::STATUS_PROCESSING, $order->getStatus());
+        $this->assertNotNull($order->getOfferAcceptedAt());
 
     }
 
     public function testMarkAsConnecting()
     {
+        $client = Client::create("client@example.com", "ey$.sadasd123asd123");
+        $order = new Order(Uuid::uuid4()->toString(),
+            new Place("McDonald","123.123","321.321","McDonald st"),
+            new Place("Home","321.321","123.123","Home st"),
+            $client,"");
+        $product = $order->addProduct($client, "new product", "5");
+        $order->markAsPublishedByClient($client);
+        $driver = Driver::create("driver@example.com", "ey$.2312313");
+        $offer = $driver->makeOffer($order, "55");
+        $order->markAsProcessing($client, $offer);
+        $order->markAsConnecting($driver);
+        $this->assertEquals(Order::STATUS_CONNECTING, $order->getStatus());
+        $this->assertNotNull($order->getDriverArrivedAt());
+    }
 
+    public function testMarkAsDelivered()
+    {
+        $client = Client::create("client@example.com", "ey$.sadasd123asd123");
+        $order = new Order(Uuid::uuid4()->toString(),
+            new Place("McDonald","123.123","321.321","McDonald st"),
+            new Place("Home","321.321","123.123","Home st"),
+            $client,"");
+        $product = $order->addProduct($client, "new product", "5");
+        $order->markAsPublishedByClient($client);
+        $driver = Driver::create("driver@example.com", "ey$.2312313");
+        $offer = $driver->makeOffer($order, "55");
+        $order->markAsProcessing($client, $offer);
+        $order->markAsConnecting($driver);
+        $order->markAsDelivered($driver);
+        $this->assertEquals(Order::STATUS_DELIVERED, $order->getStatus());
+        $this->assertNotNull($order->getFinishedAt());
     }
 
     public function testMarkAsFailed()
     {
-
+        $client = Client::create("client@example.com", "ey$.sadasd123asd123");
+        $order = new Order(Uuid::uuid4()->toString(),
+            new Place("McDonald","123.123","321.321","McDonald st"),
+            new Place("Home","321.321","123.123","Home st"),
+            $client,"");
+        $product = $order->addProduct($client, "new product", "5");
+        $order->markAsPublishedByClient($client);
+        $driver = Driver::create("driver@example.com", "ey$.2312313");
+        $offer = $driver->makeOffer($order, "55");
+        $order->markAsProcessing($client, $offer);
+        $order->markAsConnecting($driver);
+        //Should wait 10 minutes from arrival time before mark as failed
+        $this->expectException(InvalidActionForCurrentOrderState::class);
+        $order->markAsFailed($driver);
     }
 }
